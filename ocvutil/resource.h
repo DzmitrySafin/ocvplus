e9693590ca82aa0f903bd353002306767887ffc2
Dzmitry Safin <dzmitry.safin@gmail.com>
2018-12-19 17:46:44 +0300

    Refactoring project in Visual Studio

--- ocvutil/resource.h
+++ ocvutil/resource.h
@@ -2,7 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by ocvutil.rc
 //
-#define IDB_BITMAP1                     101
 #define IDB_BITMAP_CUBE                 101
 
 // Next default values for new objects
